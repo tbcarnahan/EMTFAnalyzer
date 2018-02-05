f310bf5c9b00015785bf227ae9fe94781ac0d534
Wei Shi <weishi@rice.edu>
2018-02-05 20:15:48 +0100

    Update RecoMuonInfo.h

--- NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h
+++ NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h
@@ -17,11 +17,9 @@ struct RecoMuonInfo {
   std::vector<TString> ints = {{"nRecoMuons"}};
   std::vector<TString> vFlt = {{"reco_pt", "reco_eta", "reco_phi", "reco_St1_eta", "reco_St1_phi", "reco_St2_eta", "reco_St2_phi"}};
   std::vector<TString> vInt = {{"reco_charge", "reco_loose", "reco_medium", "reco_tight"}};
-  std::vector<TString> vvInt = {{"trk_iHit"}};
   std::map<TString, int> mInts;
   std::map<TString, std::vector<float> > mVFlt;
   std::map<TString, std::vector<int> > mVInt;
-  std::map<TString, std::vector<std::vector<int> > > mVVInt;
 
   void Initialize();
   void Reset();
