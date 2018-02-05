9bfbc9031d3595e0eb44c48b8bacf71554501720
Wei Shi <weishi@rice.edu>
2018-02-05 15:56:25 +0100

    Update RecoMuonDataFormat.h

--- NTupleMaker/interface/FlatNtupleBranches/RecoMuonDataFormat.h
+++ NTupleMaker/interface/FlatNtupleBranches/RecoMuonDataFormat.h
@@ -17,23 +17,13 @@
     {
     nMuons=0;
 
-    e.clear();
-    et.clear();
     pt.clear();
     eta.clear();
     phi.clear();
     isLooseMuon.clear();
     isMediumMuon.clear();
     isTightMuon.clear();
-    iso.clear();
-    hlt_isomu.clear();
-    hlt_mu.clear();
-    hlt_isoDeltaR.clear();
-    hlt_deltaR.clear();
-    passesSingleMuon.clear();
     charge.clear();
-    mt.clear();
-    met.clear();
     etaSt1.clear();
     phiSt1.clear();
     etaSt2.clear();
@@ -41,23 +31,13 @@
     }
 
     unsigned short nMuons;
-    std::vector<float> e;
-    std::vector<float> et;
     std::vector<float> pt;
     std::vector<float> eta;
     std::vector<float> phi;
     std::vector<bool> isLooseMuon;
     std::vector<bool> isMediumMuon;
     std::vector<bool> isTightMuon;
-    std::vector<float> iso;
-    std::vector<short> hlt_isomu;
-    std::vector<short> hlt_mu;
-    std::vector<float> hlt_isoDeltaR;
-    std::vector<float> hlt_deltaR;
-    std::vector<int> passesSingleMuon;
     std::vector<int> charge;
-    std::vector<double> met;
-    std::vector<double> mt;
     std::vector<float> etaSt1;
     std::vector<float> phiSt1;
     std::vector<float> etaSt2;
