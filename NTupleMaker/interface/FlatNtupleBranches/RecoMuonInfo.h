ba20cc39069e7a61f4b6156cf5a17139e6d6a7d5
Wei Shi <weishi@rice.edu>
2018-02-05 20:13:16 +0100

    Update RecoMuonInfo.h

--- NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h
+++ NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h
@@ -2,64 +2,30 @@
 #define FlatNtupleBranchesRecoMuonInfo_h
 
 //-------------------------------------------------------------------------------
-// Adapted from cmssw/L1Trigger/L1TNtuples/interface/L1AnalysisRecoMuon2.h
-// Wei Shi
+// Create by Wei Shi
 //-------------------------------------------------------------------------------
 // Common branch info
 #include "EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/Common.h"
 
-#include "JetMETCorrections/JetCorrector/interface/JetCorrector.h"
-#include "DataFormats/JetReco/interface/CaloJetCollection.h"
-#include "DataFormats/JetReco/interface/JetID.h"
-#include "EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/RecoMuonDataFormat.h"
+// EMTF classes
+#include "L1Trigger/L1TNtuples/interface/L1AnalysisRecoMuon2.h"
 
-//muons
-#include "DataFormats/MuonReco/interface/Muon.h"
-#include "DataFormats/MuonReco/interface/MuonFwd.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/TrackReco/interface/TrackFwd.h"
-#include "DataFormats/GeometrySurface/interface/Cylinder.h"
-#include "DataFormats/GeometrySurface/interface/Plane.h"
-#include "DataFormats/MuonReco/interface/MuonEnergy.h"
-#include "DataFormats/MuonReco/interface/MuonTime.h"
-#include "CondFormats/AlignmentRecord/interface/TrackerSurfaceDeformationRcd.h"
+////////////////////////////////
+///  EMTF track information  ///
+////////////////////////////////
+struct RecoMuonInfo {
+  std::vector<TString> ints = {{"nRecoMuons"}};
+  std::vector<TString> vFlt = {{"reco_pt", "reco_eta", "reco_phi", "reco_St1_eta", "reco_St1_phi", "reco_St2_eta", "reco_St2_phi"}};
+  std::vector<TString> vInt = {{"reco_charge", "reco_loose", "reco_medium", "reco_tight"}};
+  std::vector<TString> vvInt = {{"trk_iHit"}};
+  std::map<TString, int> mInts;
+  std::map<TString, std::vector<float> > mVFlt;
+  std::map<TString, std::vector<int> > mVInt;
+  std::map<TString, std::vector<std::vector<int> > > mVVInt;
 
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/EventSetup.h"
-
-
-//vertices bp
-#include "DataFormats/VertexReco/interface/Vertex.h"
-#include "DataFormats/VertexReco/interface/VertexFwd.h"
-#include "EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/RecoVertexDataFormat.h"
-
-// track extrapolation
-#include "MuonAnalysis/MuonAssociators/interface/PropagateToMuon.h"
-#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
-
-  class RecoMuonInfo
-  {
-  public:
-    RecoMuonInfo(const edm::ParameterSet& pset);
-    ~RecoMuonInfo();
-    
-    void init(const edm::EventSetup &eventSetup);
-
-    //void Print(std::ostream &os = std::cout) const;
-    void SetMuon(const edm::Event& event,
-                 const edm::EventSetup& setup,
-                 const edm::Handle<reco::MuonCollection> muons,
-                 const edm::Handle<reco::VertexCollection> vertices,
-                 unsigned maxMuon);
-
-    RecoMuonDataFormat * getData() {return &recoMuon_;}
-    void Reset() {recoMuon_.Reset();}
-
-  private :
-    RecoMuonDataFormat recoMuon_;
-
-    PropagateToMuon muPropagator1st_;
-    PropagateToMuon muPropagator2nd_;
-  }; 
+  void Initialize();
+  void Reset();
+  void Fill(const L1Analysis::L1AnalysisRecoMuon2 & recoMuon_);
+};
 
 #endif
