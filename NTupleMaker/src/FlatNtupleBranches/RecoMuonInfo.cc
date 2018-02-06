a59de1f85fdf76319d037716b8be09e319e7be52
Wei Shi <weishi@rice.edu>
2018-02-06 19:29:10 +0100

    Update RecoMuonInfo.cc

--- NTupleMaker/src/FlatNtupleBranches/RecoMuonInfo.cc
+++ NTupleMaker/src/FlatNtupleBranches/RecoMuonInfo.cc
@@ -17,15 +17,15 @@ void RecoMuonInfo::Reset(){
 
 void RecoMuonInfo::Fill(L1Analysis::L1AnalysisRecoMuon2DataFormat* recoMuonData){
 	INSERT(mInts, "nRecoMuons", ACCESS(mVFlt, "reco_pt").size() + 1);
-	INSERT(mVFlt, "reco_pt", recoMuonData->pt );
-	INSERT(mVFlt, "reco_eta", recoMuonData.eta );
+	INSERT(mVFlt, "reco_pt", recoMuonData->pt[1] );
+	INSERT(mVFlt, "reco_eta", recoMuonData->eta[1] );
 	INSERT(mVFlt, "reco_phi", recoMuonData->phi[1] );
-	INSERT(mVInt, "reco_charge", recoMuonData()->charge[1] );
-	INSERT(mVInt, "reco_loose", recoMuonData()->isLooseMuon[1] );
-	INSERT(mVInt, "reco_medium", recoMuonData()->isMediumMuon[1] );
-	INSERT(mVInt, "reco_tight", recoMuonData()->isTightMuon[1] );
-	INSERT(mVFlt, "reco_St1_eta", recoMuonData()->etaSt1[1] );//initial -9999
-	INSERT(mVFlt, "reco_St1_phi", recoMuonData()->phiSt1[1] );
-	INSERT(mVFlt, "reco_St2_eta", recoMuonData()->etaSt2[1] );
-	INSERT(mVFlt, "reco_St2_phi", recoMuonData()->phiSt2[1] );
+	INSERT(mVInt, "reco_charge", recoMuonData->charge[1] );
+	INSERT(mVInt, "reco_loose", recoMuonData->isLooseMuon[1] );
+	INSERT(mVInt, "reco_medium", recoMuonData->isMediumMuon[1] );
+	INSERT(mVInt, "reco_tight", recoMuonData->isTightMuon[1] );
+	INSERT(mVFlt, "reco_St1_eta", recoMuonData->etaSt1[1] );//initial -9999
+	INSERT(mVFlt, "reco_St1_phi", recoMuonData->phiSt1[1] );
+	INSERT(mVFlt, "reco_St2_eta", recoMuonData->etaSt2[1] );
+	INSERT(mVFlt, "reco_St2_phi", recoMuonData->phiSt2[1] );
 }
