ea268036fef0c58f0963ffb819c8248f52881245
Wei Shi <weishi@rice.edu>
2018-02-06 19:03:30 +0100

    Update RecoMuonInfo.h

--- NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h
+++ NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h
@@ -23,7 +23,7 @@ struct RecoMuonInfo {
 
   void Initialize();
   void Reset();
-  void Fill(L1Analysis::L1AnalysisRecoMuon2 & recoMuon);
+  void Fill(L1Analysis::L1AnalysisRecoMuon2DataFormat * recoMuonData);
 };
 
 #endif
