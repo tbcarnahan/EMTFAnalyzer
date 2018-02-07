feaa671efe13e0f01a39dd7e4e6b04d3d2830d1e
Wei Shi <weishi@rice.edu>
2018-02-07 15:10:41 +0100

    Update FlatNtuple_cfi.py

--- NTupleMaker/python/FlatNtuple_cfi.py
+++ NTupleMaker/python/FlatNtuple_cfi.py
@@ -16,5 +16,19 @@ FlatNtupleData = cms.EDAnalyzer('FlatNtuple',
                                 emtfUnpTrackTag  = cms.InputTag("emtfStage2DigisData"),
                                 recoMuTag  = cms.InputTag("muons"),
                                 verticesTag = cms.InputTag("offlinePrimaryVertices"),
-                                MetTag = cms.InputTag("pfMet"),
-                                )
+                                # muon track extrapolation to 1st station
+                                muProp1st = cms.PSet(
+                                  useTrack = cms.string("tracker"),  # 'none' to use Candidate P4; or 'tracker', 'muon', 'global'
+                                  useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra
+                                  useSimpleGeometry = cms.bool(True),
+                                  useStation2 = cms.bool(False),
+                                ),
+                                # muon track extrapolation to 2nd station
+                                muProp2nd = cms.PSet(
+                                  useTrack = cms.string("tracker"),  # 'none' to use Candidate P4; or 'tracker', 'muon', 'global'
+                                  useState = cms.string("atVertex"), # 'innermost' and 'outermost' require the TrackExtra
+                                  useSimpleGeometry = cms.bool(True),
+                                  useStation2 = cms.bool(True),
+                                  fallbackToME1 = cms.bool(False),
+                                ),
+                               )
