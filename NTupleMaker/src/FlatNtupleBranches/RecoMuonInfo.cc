86d4d3de8aedcb0d34be4f4404bf357b5d836676
Wei Shi <weishi@rice.edu>
2018-02-07 13:21:22 +0100

    Update RecoMuonInfo.cc

--- NTupleMaker/src/FlatNtupleBranches/RecoMuonInfo.cc
+++ NTupleMaker/src/FlatNtupleBranches/RecoMuonInfo.cc
@@ -1,6 +1,8 @@
 //Added by Wei Shi
 
 #include "EMTFAnalyzer/NTupleMaker/interface/FlatNtupleBranches/RecoMuonInfo.h"
+#include <DataFormats/PatCandidates/interface/Muon.h>
+#include "L1Trigger/L1TNtuples/interface/MuonID.h"
 
 void RecoMuonInfo::Initialize() {
   for (auto & str : ints)  mInts .insert( std::pair<TString, int>(str, DINT) );
@@ -15,17 +17,42 @@ void RecoMuonInfo::Reset(){
 	INSERT(mInts, "nRecoMuons", 0);
 }
 
-void RecoMuonInfo::Fill(L1Analysis::L1AnalysisRecoMuon2DataFormat* recoMuonData){
+void RecoMuonInfo::Fill(reco::Muon & it){
 	INSERT(mInts, "nRecoMuons", ACCESS(mVFlt, "reco_pt").size() + 1);
-	INSERT(mVFlt, "reco_pt", recoMuonData->pt[1] );
-	INSERT(mVFlt, "reco_eta", recoMuonData->eta[1] );
-	INSERT(mVFlt, "reco_phi", recoMuonData->phi[1] );
-	INSERT(mVInt, "reco_charge", recoMuonData->charge[1] );
-	INSERT(mVInt, "reco_loose", recoMuonData->isLooseMuon[1] );
-	INSERT(mVInt, "reco_medium", recoMuonData->isMediumMuon[1] );
-	INSERT(mVInt, "reco_tight", recoMuonData->isTightMuon[1] );
-	INSERT(mVFlt, "reco_St1_eta", recoMuonData->etaSt1[1] );//initial -9999
-	INSERT(mVFlt, "reco_St1_phi", recoMuonData->phiSt1[1] );
-	INSERT(mVFlt, "reco_St2_eta", recoMuonData->etaSt2[1] );
-	INSERT(mVFlt, "reco_St2_phi", recoMuonData->phiSt2[1] );
+	INSERT(mVFlt, "reco_pt", it->pt() );
+	INSERT(mVFlt, "reco_eta", it->eta() );
+	INSERT(mVFlt, "reco_phi", it->phi() );
+	INSERT(mVInt, "reco_charge", it->charge() );
+	//check isLooseMuon
+        bool flagLoose = isLooseMuonCustom(*it);
+	INSERT(mVInt, "reco_loose", flagLoose );
+
+     	//check isMediumMuon
+     	bool flagMedium = isMediumMuonCustom(*it);
+    	INSERT(mVInt, "reco_medium", flagMedium );
+      
+    	//check isTightMuon
+    	bool flagTight = false;
+    	if (vertices.isValid()) flagTight = isTightMuonCustom(*it, (*vertices)[0]);
+	INSERT(mVInt, "reco_tight", flagTight );
+	
+	// extrapolation of track coordinates
+    	TrajectoryStateOnSurface stateAtMuSt1 = muPropagator1st_.extrapolate(*it);
+    	if (stateAtMuSt1.isValid()) {
+		INSERT(mVFlt, "reco_St1_eta", stateAtMuSt1.globalPosition().eta() );
+		INSERT(mVFlt, "reco_St1_phi", stateAtMuSt1.globalPosition().phi() );
+    	} else {
+		INSERT(mVFlt, "reco_St1_eta", -9999 );
+		INSERT(mVFlt, "reco_St1_phi", -9999 );
+    	}
+
+    	TrajectoryStateOnSurface stateAtMuSt2 = muPropagator2nd_.extrapolate(*it);
+    	if (stateAtMuSt2.isValid()) {
+		INSERT(mVFlt, "reco_St2_eta", stateAtMuSt2.globalPosition().eta() );
+		INSERT(mVFlt, "reco_St2_phi", stateAtMuSt2.globalPosition().phi() );
+    	} else {
+		INSERT(mVFlt, "reco_St2_eta", -9999 );
+		INSERT(mVFlt, "reco_St2_phi", -9999 );
+    	}
+	
 }
