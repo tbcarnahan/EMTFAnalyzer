727b63c331b7231e3e6b82516bcbc8f9d5b1a36c
Wei Shi <weishi@rice.edu>
2018-02-12 21:27:21 +0100

    Update RecoTrkMatcher.h

--- NTupleMaker/interface/MatcherDR/RecoTrkMatcher.h
+++ NTupleMaker/interface/MatcherDR/RecoTrkMatcher.h
@@ -16,15 +16,14 @@
 ////////////////////////////////
 
 struct RecoTrkMatcher {
-  std::vector<TString> vFlt = {{"reco_match_trk_dR", "reco_match_trk_dPhi", "reco_match_trk_dEta",
-                                "trk_match_reco_dR", "trk_match_reco_dPhi", "trk_match_reco_dEta"}};
-  std::vector<TString> vInt = {{"reco_match_iTrk", "trk_match_iReco"}};
+  std::vector<TString> vFlt = {{"reco_match_trk_dR", "reco_match_trk_dPhi", "reco_match_trk_dEta"}};
+  std::vector<TString> vInt = {{"reco_match_iTrk"}};
   std::map<TString, std::vector<float> > mVFlt;
   std::map<TString, std::vector<int> > mVInt;
   
   void Initialize();
   void Reset();
-  void Fill(const RecoMuonInfo & recoMuons, const EMTFTrackInfo & emtfTrks);
+  void Fill(const RecoMuonInfo & recoMuons, const EMTFTrackInfo & emtfTrks, const float min_eta, const float max_eta);
 };
 
 #endif
